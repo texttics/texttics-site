b003a50acb6e759a14d26e4d81cd86a3f554e845
texttics <texttics@gmail.com>
2025-12-04 19:43:25 +0100

    Update app.py

--- app.py
+++ app.py
@@ -14964,6 +14964,9 @@ def analyze_html_metadata(raw_html_string: str):
         
         # Push to UI
         _update_css_workbench_ui(ledger, audited_findings, ghost_html)
+
+        # This plugs the function into the browser's global scope
+        window.analyze_html_metadata = analyze_html_metadata
         
         return audited_findings
 
